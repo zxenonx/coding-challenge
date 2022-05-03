14eecd628d3be3575997bd6678d0da4027b4033c
xzenon <forlanfr_8@hotmail.fr>
2022-05-03 14:00:33 +0100

    Create main.py
    Implement startup function init_db()

--- app/main.py
+++ app/main.py
@@ -1,6 +1,16 @@
 from fastapi import FastAPI
 
+import models
+
+from db.session import engine
+
 app = FastAPI(
     debug=True,
     title="Grades|Dataset"
 )
+
+@app.on_event("startup")
+def init_db():
+    """Creates table defined in models.py.
+    """
+    models.Base.metadata.create_all(engine)
