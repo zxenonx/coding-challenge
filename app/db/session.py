a15115558b30ce6567d8546de56a123cdec57237
xzenon <forlanfr_8@hotmail.fr>
2022-05-06 19:54:54 +0100

    Implement function to insert dataset into db

--- app/db/session.py
+++ app/db/session.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 from config import settings
+import pandas as pd
 
 engine = create_engine(settings.DATABASE_URL)
 
@@ -10,4 +11,28 @@ SessionLocal = sessionmaker(autoflush=False, bind=engine)
 
 Base = declarative_base()
 
+table = "grades"
 
+
+def write_to_db(dataframe: pd.DataFrame) -> None:
+    """_summary_
+
+    Args:
+        dataframe (pd.DataFrame): _description_
+    """
+    rows_affected = 0
+    connection = engine.connect()
+    try:
+        rows_affected = dataframe.to_sql(
+            name=table,
+            con=connection,
+            if_exists="replace",
+            index=True,
+            index_label="id"
+        )
+    except ValueError as error:
+        raise(error)
+    else:
+        print("data", rows_affected)
+    finally:
+        connection.close()
