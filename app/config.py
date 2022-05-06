1411cfe49c0a72bbc4d251d29b7e01e560ec034b
xzenon <forlanfr_8@hotmail.fr>
2022-05-06 14:51:45 +0100

    Add DATASET_URL

--- app/config.py
+++ app/config.py
@@ -6,15 +6,20 @@ from pathlib import Path
 env_path = Path(".env")
 load_dotenv(dotenv_path=env_path)
 
+
 class Settings(BaseSettings):
-    """Holds the configuration variables of the project.
-    """
+    """Holds the configuration variables of the project."""
 
     PROJECT_NAME = "Grades Dataset"
     DEBUG = True
+    DATASET_URL = (
+        "https://data.cityofnewyork.us/api/views/7yc5-fec2/rows.csv?accessType=DOWNLOAD"
+    )
 
     POSTGRES_USER = os.getenv("POSTGRES_USER")
-    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD",)
+    POSTGRES_PASSWORD = os.getenv(
+        "POSTGRES_PASSWORD",
+    )
     POSTGRES_SERVER = os.getenv("POSTGRES_SERVER")
     POSTGRES_PORT = os.getenv("POSTGRES_PORT", 5432)
     POSTGRES_DB = os.getenv("POSTGRES_DB")
