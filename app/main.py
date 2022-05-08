f38bb899a973a5d7a178c7d4b2158c2cfbd12f31
xzenon <forlanfr_8@hotmail.fr>
2022-05-08 15:13:23 +0100

    Load the dataset at startup

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
 from fastapi import FastAPI
+from worker import load_dataset
+from config import settings
 
 import models
 
 from db.session import engine
 
-app = FastAPI(
-    debug=True,
-    title="Grades|Dataset"
-)
+app = FastAPI(debug=True, title="Grades|Dataset")
+
 
 @app.on_event("startup")
-def init_db():
-    """Creates table defined in models.py.
-    """
+async def init_db():
+    """Creates table defined in models.py."""
     models.Base.metadata.create_all(engine)
+
+    # load dataset into table "grades"
+    load_dataset.delay(settings.DATASET_URL)
